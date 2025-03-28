3770cf0b553858d2923c4141594028cc3ec8a1e4
John Andrade <sistemas@johnandrade.com>
2025-03-28 12:28:05 -0500

    first commit

--- frontend/src/stores/item.store.ts
+++ frontend/src/stores/item.store.ts
@@ -2,6 +2,8 @@ import { defineStore } from "pinia";
 import axios from "axios";
 import type { Item } from "../type/data";
 
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+
 export const useItemStore = defineStore("item", {
   state: () => ({
     items: [],
@@ -10,21 +12,19 @@ export const useItemStore = defineStore("item", {
   }),
   actions: {
     async fetchItems() {
-      const response = await axios.get(
-        "https://fullstack-a1bh.onrender.com/items"
-      );
+      const response = await axios.get(`${API_URL}`);
       this.items = response.data;
     },
     async addItem(item: Item) {
-      await axios.post("https://fullstack-a1bh.onrender.com/items", item);
+      await axios.post(`${API_URL}`, item);
       await this.fetchItems();
     },
     async updateItem(id: string, item: Item) {
-      await axios.put(`https://fullstack-a1bh.onrender.com/items/${id}`, item);
+      await axios.put(`${API_URL}/${id}`, item);
       await this.fetchItems();
     },
     async deleteItem(id: string): Promise<void> {
-      await axios.delete(`https://fullstack-a1bh.onrender.com/items/${id}`);
+      await axios.delete(`${API_URL}/${id}`);
       await this.fetchItems();
     },
     openDialog(item = null) {
