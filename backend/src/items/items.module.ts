da123608cd22d42ddaea8ade50aa191bb611239c
John Andrade <sistemas@johnandrade.com>
2025-03-28 11:34:55 -0500

    first commit

--- backend/src/items/items.module.ts
+++ backend/src/items/items.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ItemService } from './items.service';
 import { ItemController } from './items.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Item } from './entities/item.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Item]), // Aquí puedes agregar tus entidades si es necesario
+  ],
   controllers: [ItemController],
   providers: [ItemService],
 })
